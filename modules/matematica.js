8e1d50dea30178e5f0e5c65ddd7f6336a2dbb56a
Android Studio <devandroid@ort>
2026-04-06 09:35:26 -0300

    primer commit

---
+++ modules/matematica.js
@@ -0,0 +1,21 @@
+/* Este es el módulo "matematicas" */
+const PI = 3.14;
+function sumar(x, y) {
+    return x + y;
+}
+
+function restar(x, y) {
+    return x - y;
+}
+
+const multiplicar = (a, b) => {
+    return a * b;
+};
+
+const dividir = (a, b) => {
+    return a / b;
+};
+const arrayNumeros = ["dos", "cuatro", "ocho", "diez"];
+
+// Exporto todo lo que yo quiero exponer del módulo hacia el exterior.
+export { PI, sumar, restar, multiplicar, dividir, numeros };
